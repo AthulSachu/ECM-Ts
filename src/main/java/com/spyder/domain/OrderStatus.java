7990b06b00823d4efe379397626fcf91b54798f0
AthulSachu <athulsachu80@gmail.com>
2025-01-29 23:38:39 +0530

    new enum added

---
+++ src/main/java/com/spyder/domain/OrderStatus.java
@@ -0,0 +1,10 @@
+package com.spyder.domain;
+
+public enum OrderStatus {
+    PENDING,
+    PLACED,
+    CONFIRMED,
+    SHIPPED,
+    DELIVERED,
+    CANCELLED
+}
---
+++ src/main/java/com/spyder/domain/PaymentStatus.java
@@ -0,0 +1,8 @@
+package com.spyder.domain;
+
+public enum PaymentStatus {
+    PENDING,
+    PROCESSING,
+    COMPLETED,
+    FAILED
+}
