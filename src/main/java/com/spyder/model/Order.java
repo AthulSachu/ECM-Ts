1bd710f8a8adf99469f249230001ee818497f1d5
AthulSachu <athulsachu80@gmail.com>
2025-01-29 23:38:56 +0530

    new class added

---
+++ src/main/java/com/spyder/model/Order.java
@@ -0,0 +1,54 @@
+package com.spyder.model;
+
+import com.spyder.domain.OrderStatus;
+import com.spyder.domain.PaymentStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class Order {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String orderId;
+
+    @ManyToOne
+    private User user;
+
+    private Long sellerId;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    @ManyToOne
+    private Address shippingAddress;
+
+    @Embedded
+    private PaymentDetails paymentDetails = new PaymentDetails();
+
+    private double totalMrpPrice;
+
+    private Integer totalSellingPrice;
+
+    private Integer discount;
+
+    private OrderStatus orderStatus;
+
+    private int totalItem;
+
+    private PaymentStatus paymentStatus = PaymentStatus.PENDING;
+
+    private LocalDateTime orderDate = LocalDateTime.now();
+
+    private LocalDateTime deliverDate = orderDate.plusDays(7);
+}
---
+++ src/main/java/com/spyder/model/Wishlist.java
@@ -0,0 +1,4 @@
+package com.spyder.model;
+
+public class Wishlist {
+}
